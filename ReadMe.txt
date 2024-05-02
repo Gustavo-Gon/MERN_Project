8fd4c13d603576c63ad2749b6863e1285e909284
Gustavo-Gon <gon.gustavo@outlook.com>
2024-05-02 00:59:59 -0700

    Login page design

--- ReadMe.txt
+++ ReadMe.txt
@@ -4,3 +4,6 @@ MongoDB — document database
 Express(.js) — Node.js web framework
 React(.js) — a client-side JavaScript framework
 Node(.js) — the premier JavaScript web server (runtime)
+
+Frontend:
+npm start 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,67 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // State hooks for username and password
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Handle input change
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === 'username') {
+      setUsername(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  // Handle for submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Logging in with:', username, password);
+    // Add in backend service 
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background">
+      <div className="login-box">
+      <h1 className="cursive-text">TrailBlazers</h1>
+
+        <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <input 
+            type="text" 
+            placeholder="Username"
+            className="form-control" 
+            name="username"
+            value={username}
+            onChange={handleInputChange}
+            />
+        </div>
+
+        <div className="form-group">
+          <input 
+            type="password" 
+            placeholder="Password" 
+            className="form-control" 
+            name="password"
+            value={password}
+            onChange={handleInputChange}
+            />
+        </div>
+
+        <button type="submit" className="btn">Login</button>
+        </form>
+
+        <div className="login-help">
+          <a href="#" className="forgot-password">Forgot password?</a>
+          <a href="#" className="signup-link">Don't have an account? Sign up</a>
+        </div>
+
+      </div>
     </div>
   );
 }
